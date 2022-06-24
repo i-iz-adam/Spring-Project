6cf90feb91af97fbff059bbbff6be7aaf4ad4ef3
Adam <naxosrsps@gmail.com>
2022-06-24 20:11:38 +0100

    Spring Boot Annotations

--- src/main/java/com/decimate/site/SiteApplication.java
+++ src/main/java/com/decimate/site/SiteApplication.java
@@ -1,9 +1,15 @@
 package com.decimate.site;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan("com.decimate.site.controller")
 public class SiteApplication {
 
 	public static void main(String[] args) {
